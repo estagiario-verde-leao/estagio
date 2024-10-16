03b5c3b66229eeefb87f4ae3e6c8c8c034c5de80
lucioweb <lucio.lemos.385@ufrn.edu.br>
2024-10-16 11:27:41 -0300

    Alteração na classe sayHello

--- source/SayHello.php
+++ source/SayHello.php
@@ -6,11 +6,25 @@ namespace Source;
 
 class SayHello
 {
+    /**
+     * Um método construtor vazio é perfeitamente válido em PHP. 
+     * Serve como um ponto de entrada quando a classe é instanciada, mesmo que não faça nada. 
+     * Pode ser útil futuramente se você quiser adicionar alguma lógica de inicialização à classe.
+     */
+    public function __construct()
+    {
+        //* Code
+    }
     /**FUNÇÃO QUE RETORNA UMA SAUDAÇÃO QUANDO É INSTANCIADA
      * @return [type]
      */
     public function sayHello(): void
     {
         echo "Olá estagiário!" . PHP_EOL;
+    }
+
+    public function __destruct()
+    {
+        //* Code
     }
 }
